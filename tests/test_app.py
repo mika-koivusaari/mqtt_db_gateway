ec912de9c36b8ec67ee7b806a2599e8015fc2be0
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-11-28 14:22:37 +0200

    Added tests and requirements.txt

---
+++ tests/test_app.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+
+
+class TestApp(TestCase):
+    def test_reload_program_config(self):
+        self.fail()
+
+    def test_terminate(self):
+        self.fail()
+
+    def test_readConfig(self):
+        self.fail()
+
+    def test_program_cleanup(self):
+        self.fail()
+
+    def test_close_resources(self):
+        self.fail()
+
+    def test_createLogger(self):
+        self.fail()
+
+    def test_on_connect(self):
+        self.fail()
+
+    def test_on_message(self):
+        self.fail()
+
+    def test_exec_process(self):
+        self.fail()
+
+    def test_load_mqtt_input(self):
+        self.fail()
+
+    def test_load_rawid_sensorid(self):
+        self.fail()
+
+    def test_openConnections(self):
+        self.fail()
+
+    def test_run(self):
+        self.fail()
+
+    def test_config(self):
+        self.fail()
+
+    def test_config(self):
+        self.fail()
+
+    def test_pid(self):
+        self.fail()
+
+    def test_nodaemon(self):
+        self.fail()
+
+    def test_stop(self):
+        self.fail()
+
+    def test_reload(self):
+        self.fail()
+
+    def test_start(self):
+        self.fail()
