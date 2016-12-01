743717fcfd307df4816fc870a981ba7cc80b4939
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-12-01 16:10:17 +0200

    Added read_config tests.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
+from unittest.mock import DEFAULT
+
+from configparser import NoSectionError
+from configparser import NoOptionError
 
 from mqtt_db_gateway import App
 
@@ -13,7 +17,7 @@ class TestApp(TestCase):
 
     @patch('mqtt_db_gateway.grp')
     @patch('mqtt_db_gateway.ConfigParser')
-    def test_readConfig_fileread(self,configparsermock,grpmock):
+    def test_readConfig_fileread_givenname(self,configparsermock,grpmock):
         app = App(
             cfg='cfg',
             pid='pid',
@@ -27,6 +31,74 @@ class TestApp(TestCase):
         app.readConfig('file.ini')
 
         instance.read.assert_called_with('file.ini')
+
+    @patch('mqtt_db_gateway.grp')
+    @patch('mqtt_db_gateway.ConfigParser')
+    def test_readConfig_fileread_noname(self, configparsermock, grpmock):
+        app = App(
+            cfg='cfg',
+            pid='pid',
+            nodaemon=True
+        )
+
+        instance = configparsermock.return_value
+
+        grpmock.getgrnam.return_value(('name', 'passwd', 1, 'users'))
+
+        app.readConfig(None)
+
+        instance.read.assert_called_with('mqtt_db_gateway.ini')
+
+    @patch('mqtt_db_gateway.grp')
+    @patch('mqtt_db_gateway.ConfigParser')
+    def test_readConfig_mandatorysection_nodaemon(self, configparsermock, grpmock):
+        app = App(
+            cfg='cfg',
+            pid='pid',
+            nodaemon=True
+        )
+
+        instance = configparsermock.return_value
+
+        grpmock.getgrnam.return_value(('name', 'passwd', 1, 'users'))
+
+        def get_side_effect(section,key):
+            if section == 'sqlalchemy' and key == 'uri':
+                raise NoSectionError('Mandatory config test')
+            else:
+                return DEFAULT
+
+        instance.get.side_effect = get_side_effect
+        self.assertRaises(SystemExit, app.readConfig,None)
+
+    @patch('mqtt_db_gateway.App.logger')
+    @patch('mqtt_db_gateway.DaemonContext')
+    @patch('mqtt_db_gateway.grp')
+    @patch('mqtt_db_gateway.ConfigParser')
+    def test_readConfig_mandatorysection_daemon(self, configparsermock, grpmock, daemonmock, loggermock):
+        app = App(
+            cfg='cfg',
+            pid='pid',
+            nodaemon=False
+        )
+
+        daemonmock.is_open.return_value = True
+        app.daemon=daemonmock
+
+        instance = configparsermock.return_value
+
+        grpmock.getgrnam.return_value(('name', 'passwd', 1, 'users'))
+
+        def get_side_effect(section, key):
+            print (section + ', ' + key)
+            if section == 'sqlalchemy' and key == 'uri':
+                print("error")
+                raise NoSectionError('Mandatory config test')
+            else:
+                return DEFAULT
+
+        instance.get.side_effect = get_side_effect
+        self.assertIsNone(app.readConfig(conf_file=None))
 
     def test_program_cleanup(self):
         self.fail()
